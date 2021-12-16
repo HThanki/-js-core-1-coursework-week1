29c76de810a2362d653d5702ea1458dcf516ef5f
Harsheek Thanki <harsheekthanki@gmail.com>
2021-12-16 18:28:02 +0000

    1 and 2 extra challengs complete

--- extra/2-piping.js
+++ extra/2-piping.js
@@ -6,8 +6,7 @@
   - one that multiplies 2 numbers together
   - one that formats a number so it's returned as a string with a £ sign before it (e.g. 20 -> £20)
 
-  2. Using the variable startingValue as input, perform the following operations using your functions all
-  on one line (assign the result to the variable badCode):
+  2. Using the variable startingValue as input, perform the following operations using your functions all on one line (assign the result to the variable badCode):
   - add 10 to startingValue
   - multiply the result by 2
   - format it
@@ -16,27 +15,26 @@
   the final result to the variable goodCode
 */
 
-function add() {
-
+function add(a, b) {
+  return a + b;
 }
 
-function multiply() {
-
+function multiply(a, b) {
+  return a * b;
 }
 
-function format() {
-
+function format(number) {
+  return `£${number}`;
 }
 
-const startingValue = 2;
+const startingValue = (2 + 10) * 2;
 
 // Why can this code be seen as bad practice? Comment your answer.
-let badCode =
+let badCode = format(startingValue);
 
 /* BETTER PRACTICE */
 
-let goodCode =
-
+let goodCode = format(multiply(add(2, 10), 2));
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
 
